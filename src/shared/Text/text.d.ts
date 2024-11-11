d9fb2f7ee4c80aaf88c0b95219042d54b56ab8a6
José Ramón <jose.1999.rfg@gmail.com>
2024-11-10 20:23:00 -0500

    feat(shared): create text component
    -  component name: Text
    -  styling text component

---
+++ src/shared/Text/text.d.ts
@@ -0,0 +1,12 @@
+import React from "react"
+
+interface  iTextProps {
+    
+    value: string;
+    color: string;
+    size: string;
+    font: string = "Helvetica Compressed";    
+}
+
+
+export default iTextProps;
