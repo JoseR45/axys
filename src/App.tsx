2da862e34ecfceb8e3278488f49d633cb0ec1c6c
josecma <jcma100298@gmail.com>
2024-11-12 21:36:30 -0500

    feat(shared):create horizontal line component
    - component name: HR
    
    - to mark the beginning of the sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
-import { LineTextComponent } from "./shared/LineTextComponent/line_text_component";
+import { HR } from "./shared/HR/hr";
+import { Ticker } from "./shared/Ticker/ticker";
 
 function App() {
   return (
-    <>
-      <LineTextComponent text="albondiga" />
-    </>
+    <div>
+      <Ticker speed={100} from="right">
+        <p style={{ fontSize: "3rem" }}>BACKEND.</p>
+        <p style={{ fontSize: "3rem" }}>FRONTEND.</p>
+        <p style={{ fontSize: "3rem" }}>DEVOPS.</p>
+        <p style={{ fontSize: "3rem" }}>DESIGN.</p>
+        <p style={{ fontSize: "3rem" }}>DATABASE.</p>
+        <p style={{ fontSize: "3rem" }}>ANALIST.</p>
+      </Ticker>
+
+      <Ticker speed={100} from="left">
+        <p style={{ fontSize: "3rem" }}>BACKEND.</p>
+        <p style={{ fontSize: "3rem" }}>FRONTEND.</p>
+        <p style={{ fontSize: "3rem" }}>DEVOPS.</p>
+        <p style={{ fontSize: "3rem" }}>DESIGN.</p>
+        <p style={{ fontSize: "3rem" }}>DATABASE.</p>
+        <p style={{ fontSize: "3rem" }}>ANALIST.</p>
+      </Ticker>
+      <HR from="center" />
+    </div>
   );
 }
 
---
+++ src/shared/HR/hr.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import iHRProps from "./hr.d";
+
+export const HR: React.FC<iHRProps> = ({ from }) => {
+  return (
+    <hr
+      style={{
+        border: "none",
+        height: "1px",
+        backgroundImage: `linear-gradient(${
+          from === "left" ? "to right" : "to left"
+        }, ${
+          from === "center"
+            ? "#FFFFFF 0%, #999999 50%, #FFFFFF 100%"
+            : "#999999 0%, #FFFFFF 100%"
+        })`,
+      }}
+    />
+  );
+};
