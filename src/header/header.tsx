8e29b189849551fb4356673140cda87da3d4f32d
josecma <jcma100298@gmail.com>
2024-11-14 09:43:42 -0500

    feat(src) created header component
    - The header component contains options for navigating through the website.

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,29 +1,26 @@
 import React from "react";
+import { Text } from "../shared/Text/text";
+import { Nav } from "./components/Nav/nav";
 
 export const Header: React.FC<{}> = ({}) => {
   return (
     <>
-      <header className="bg-tertiary">
-        <div>LOGO</div>
-        <div>
-          <nav>
-            <div>
-              <a href="">
-                <p>ABOUT</p>
-              </a>
-              <a href="">
-                <p>SERVICES</p>
-              </a>
-              <a href="">
-                <p>CONTACT</p>
-              </a>
-            </div>
-            <div>
-              <div></div>
-            </div>
-          </nav>
-        </div>
-      </header>
+      <div id="header-container">
+        <header className="bg-primary w-full fixed top-0 flex flex-row justify-between items-center">
+          <div id="logo-container">
+            <Text
+              value="LOGO"
+              size="40px"
+              color="white"
+              family="Inter"
+              weight="400"
+              lineHeight="48px"
+              textAlign="left"
+            />
+          </div>
+          <Nav />
+        </header>
+      </div>
     </>
   );
 };
