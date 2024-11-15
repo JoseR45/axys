e435424c2c92c36c3c5fcd14407b68954fe1d7e2
LeudiX <leudix.rafael@gmail.com>
2024-11-15 10:05:25 -0700

    feat(shared):
    add generic button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './shared/button/button_component'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,15 +17,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button name='btn-generic' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="cool-emojis">
+        😋😏😎
       </p>
     </>
   )
