1eec5829d5ce7fc78b18794bc0f5125777bd6aa6
LeudiX <leudix.rafael@gmail.com>
2024-11-17 21:31:38 -0700

    docs(SectionEnd)!: consider update to type if not intended to extend the interface

--- src/shared/SectionEnd/section_end.d.ts
+++ src/shared/SectionEnd/section_end.d.ts
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 
-interface iSectionEndProps extends PropsWithChildren<{ from: "left" | "right" }> { }
+interface iSectionEndProps extends PropsWithChildren<{ from: "left" | "right" }> {
+    //Placeholder for future extension(If not intended to be extended you should use type instead of interface) 
+}
 
 export default iSectionEndProps;
