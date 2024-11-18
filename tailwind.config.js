789bdef8b0792785fa1ad39c389cfe6717a8dd70
LeudiX <leudix.rafael@gmail.com>
2024-11-17 21:37:07 -0700

    conf(tailwind.config.js): added backgroundImage property for custom styling over Contact section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ export default {
         'primary': '#1D1D1B', 
         'secundary': 'rgba(255, 255, 255, 0.6)',
       },
+      fontFamily :{
+        'helvetica': ['HelveticaCompressed', 'sans-serif'], //Added Helvetica Compressed font type by LeudiX
+      },
+      backgroundImage:{
+        'border-gradient':'linear-gradient(90deg, #f1f1f1 0.5%, #222325 75%)',
+        'hr-linear-gradient':'linear-gradient(to right, rgb(255, 255, 255) 0%, rgb(153, 153, 153) 50%, rgb(255, 255, 255) 100%)',
+      }
     },
   },
   plugins: [],
