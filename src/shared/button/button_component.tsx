a0bc2376a96959bbb4fe43e784f0f48ba95c2755
LeudiX <leudix.rafael@gmail.com>
2024-11-20 15:57:28 -0700

    fix(shared/butonn)!: extended button component usability with optional children prop validation

--- src/shared/button/button_component.tsx
+++ src/shared/button/button_component.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import iButtonProps from "./button_component.d";
 import "./button_component_styles.css"
 
-export const Button: React.FC<iButtonProps> = ({ value,className }) => {
-    const [count, setCount] = useState(0);
+export const Button: React.FC<iButtonProps> = ({ children, className }) => {
+    //const [count, setCount] = useState(0);
 
     return (
         <button id="btn-generic"
-            className={`${className || ''}`} // Extended functionality by LeudiX
-            onClick={() => setCount((count) => count + 1)}>
-            {value} : {count}
+            className={`${className || ''}`} //Optional className prop
+        >
+        {children}
         </button>
     )
 } 
