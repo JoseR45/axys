7eed1110b91f06734cdd79ca6bdba718a28da697
LeudiX <leudix.rafael@gmail.com>
2024-11-20 19:59:28 -0700

    fix(shared/button): added default styles to button component

--- src/shared/button/button_component.tsx
+++ src/shared/button/button_component.tsx
@@ -1,6 +1,6 @@
 //import React, { useState } from "react";
 import iButtonProps from "./button_component.d";
-import "./button_component_styles.css"
+
 
 export const Button: React.FC<iButtonProps> = ({ children, className }) => {
     //const [count, setCount] = useState(0);
@@ -8,6 +8,11 @@ export const Button: React.FC<iButtonProps> = ({ children, className }) => {
     return (
         <button id="btn-generic"
             className={`${className || ''}`} //Optional className prop
+            style={{
+                padding: "10px 30px",
+                borderRadius: "50px",
+                border: "2px solid rgba(255,255,255, 0.6)"  
+            }}
         >
         {children}
         </button>
