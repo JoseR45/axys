62d4c7328411c95fef7bd92576c96dd470679c00
josecma <jcma100298@gmail.com>
2025-01-22 18:24:12 -0500

    add(header): add items of 'Nav' component

---
+++ src/header/components/Nav/nav.data.ts
@@ -0,0 +1,6 @@
+export default [
+    { href: "#home-section", text: "Home", active: true },
+    { href: "#about-section", text: "About", active: false },
+    { href: "#services-section", text: "Services", active: false },
+    { href: "#contact-section", text: "Contact", active: false }
+];   
